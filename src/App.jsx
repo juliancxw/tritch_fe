1a1d876d6c20ce2d21a7435e2c046892d8d53f5d
gzh236 <47617174+gzh236@users.noreply.github.com>
2021-07-13 00:37:18 +0800

    file changes js to jsx, basic navbar with no functionality using MUI

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Navbar from "./Components/navbar.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
